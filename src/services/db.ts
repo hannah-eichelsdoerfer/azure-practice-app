4da89d17da375c0e097a163d8fa2c51a7b1c65e7
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-07-27 16:01:48 +1000

    add mongodb mongoose and dev dependencies to project

---
+++ src/services/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+  if (!process.env.MONGODB_URI) {
+    throw new Error("No MONGODB_URI provided");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB successfully connected 🎉");
+  } catch (error) {
+    console.log("***", error);
+    throw new Error("MongoDB connection failed");
+  }
+};
+
+export default connect;

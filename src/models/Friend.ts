3f96d88cc4616eda3f785eebb7a974159c57a078
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-07-28 02:42:09 +1000

    upate model

--- src/models/Friend.ts
+++ src/models/Friend.ts
@@ -1,28 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-interface IFriend {
+export interface IFriend {
   _id: mongoose.Schema.Types.ObjectId;
   name: string;
-  birthday: Date;
   email: string;
-  phone: string;
-  address: string;
-  bestFriend: boolean;
-  friendsSince: Date;
-  note: string;
+  dateOfBirth: Date;
+  phone?: string;
+  address?: string;
+  bestFriend?: boolean;
+  friendsSince?: Date;
+  note?: string;
 }
 
 const FriendSchema = new mongoose.Schema<IFriend>(
   {
-    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
     name: { type: String, required: true },
-    birthday: { type: Date, required: true },
     email: { type: String, unique: true, required: true },
-    phone: { type: String, required: true },
-    address: { type: String, required: true },
-    bestFriend: { type: Boolean, required: true },
-    friendsSince: { type: Date, required: true },
-    note: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    phone: { type: String },
+    address: { type: String },
+    bestFriend: { type: Boolean },
+    friendsSince: { type: Date },
+    note: { type: String },
   },
   {
     timestamps: true,
@@ -31,6 +30,6 @@ const FriendSchema = new mongoose.Schema<IFriend>(
 
 // const Friend = mongoose.model<IFriend>("Friend", FriendSchema);
 const Friend =
-  mongoose.models.Friend || mongoose.model<IFriend>("Friend", FriendSchema);
+  mongoose.models.Friend || mongoose.model<IFriend>('Friend', FriendSchema);
 
 export default Friend;

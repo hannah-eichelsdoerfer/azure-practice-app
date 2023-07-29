e4fecdfd5c863ac2e830c3fc41e62b1027c3679a
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-07-29 23:48:57 +1000

    single quotes

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // trailingComma: "es5",
   // tabWidth: 2,
-  // singleQuote: true,
-  // jsxSingleQuote: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
   plugins: ["prettier-plugin-tailwindcss"],
 };
